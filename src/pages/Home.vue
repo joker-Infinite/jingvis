2685399c0726b5ef02fd98737676ec749878bccd
nieshihao <2075259561@qq.com>
2020-10-14 10:04:50 +0800

    up

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,22 @@
 <template>
-    
+  <div class="container">
+    <div class="H_top"></div>
+    《div》
+    <div class="H_bottom"></div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
-    }
+export default {
+  name: "Home"
+};
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.container {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background: aquamarine;
+}
 </style>
