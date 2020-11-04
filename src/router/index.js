b71b092f7512298e0c7fc1ecf2cd27e1d62eba4d
nieshihao <2075259561@qq.com>
2020-11-04 16:31:03 +0800

    注释

--- src/router/index.js
+++ src/router/index.js
@@ -10,26 +10,31 @@ Vue.use(VueRouter);
 
 const routes = [
   {
+    //首页
     path: "/",
     name: "Home",
     component: Home
   },
   {
+    //二级页面
     path: "/test",
     name: "test",
     component: test
   },
   {
+    //对比页面
     path: "/testC",
     name: "test",
     component: testC
   },
   {
+    //对比页面——副本
     path: "/compared",
     name: "compared",
     component: compared
   },
   {
+    //三级页面
     path: "/three",
     name: "three",
     component: three
