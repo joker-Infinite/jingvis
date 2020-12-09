da6fc82094e409a96ab0b32904a6e99e49640a60
nieshihao <2075259561@qq.com>
2020-12-09 21:33:25 +0800

    看板地圖調整

--- src/pages/homeKanBan/children/centerMapBase.vue
+++ src/pages/homeKanBan/children/centerMapBase.vue
@@ -20,20 +20,31 @@
                 marker: [],
                 gas: [],
                 service: [],
+                gasData: [],
+                serviceData: [],
                 mapStyleArr: [
                     'amap://styles/8cb6df918ee512eae9c9198c38a40c91',
                     'amap://styles/1111cca74c703c3218b102779351f6eb',
                     'amap://styles/f1f4181c84a35130099dfd661f061466',
                 ],
                 i: 0,
-                position: []
+                position: [],
+                type: ''
             }
         },
         methods: {
             changeT(v) {
                 this.i = v;
                 this.marker = [];
-                this.initMap(this.position);
+                if (!this.type) {
+                    this.initMap(this.position)
+                }
+                if (this.type == 'service') {
+                    this.initMap(this.serviceData)
+                }
+                if (this.type == 'gas') {
+                    this.initMap(this.gasData)
+                }
             },
             initMap(position) {
                 let map = new AMap.Map('MAP', {
@@ -82,16 +93,16 @@
                     AMap.event.addListener(i, 'click', function () {
                         infoWindow.open(v, i.getPosition());
                     });
-                    let title = '方恒假日酒店<span style="font-size:11px;color:#F00;">价格:318</span>',
-                        content = [];
-                    content.push("<img src='http://tpc.googlesyndication.com/simgad/5843493769827749134'>地址：北京市朝阳区阜通东大街6号院3号楼东北8.3公里");
-                    content.push("电话：010-64733333");
-                    content.push("<a href='https://ditu.amap.com/detail/B000A8URXB?citycode=110105'>详细信息</a>");
+                    let content = [];
+                    content.push("<div style='width: 200px;text-align: center'>详细信息</div>");
+                    content.push("<div style='width: 200px;text-align: center;height: 70px'>内容</div>");
                     let infoWindow = new AMap.InfoWindow({
-                        isCustom: true,  //使用自定义窗体
-                        content: that.createWindow(title, content.join("<br/>")),
-                        offset: new AMap.Pixel(16, -45)
+                        content: content.join(""),
+                        offset: new AMap.Pixel(3, -33)
                     });
+                    infoWindow.on('close', function () {
+                        // console.log('close')
+                    })
                     i.setMap(v);
                 });
                 await new Promise(resolve => {
@@ -105,44 +116,6 @@
                         renderClusterMarker: this._renderClusterMarker
                     });
                 }, 100)
-            },
-            createWindow(title, content) {
-                let info = document.createElement("div");
-                info.className = "custom-info input-card content-window-card";
-
-                //可以通过下面的方式修改自定义窗体的宽高
-                //info.style.width = "400px";
-                // 定义顶部标题
-                let top = document.createElement("div");
-                let titleD = document.createElement("div");
-                let closeX = document.createElement("img");
-                top.className = "info-top";
-                titleD.innerHTML = title;
-                closeX.src = "https://webapi.amap.com/images/close2.gif";
-                closeX.onclick = this.closeInfoWindow;
-
-                top.appendChild(titleD);
-                top.appendChild(closeX);
-                info.appendChild(top);
-
-                // 定义中部内容
-                let middle = document.createElement("div");
-                middle.className = "info-middle";
-                middle.style.backgroundColor = 'white';
-                middle.innerHTML = content;
-                info.appendChild(middle);
-
-                // 定义底部内容
-                let bottom = document.createElement("div");
-                bottom.className = "info-bottom";
-                bottom.style.position = 'relative';
-                bottom.style.top = '0px';
-                bottom.style.margin = '0 auto';
-                let sharp = document.createElement("img");
-                sharp.src = "https://webapi.amap.com/images/sharp.png";
-                bottom.appendChild(sharp);
-                info.appendChild(bottom);
-                return info;
             },
             closeInfoWindow() {
                 this.map.clearInfoWindow();
@@ -172,28 +145,32 @@
                 context.marker.setContent(div)
             },
             queryDot(v) {
+                this.type = v;
                 let map = new AMap.Map('MAP', {
                     center: [114.286298, 30.5855],
-                    zoom: 8
+                    zoom: 8,
+                    mapStyle: this.mapStyleArr[this.i]
                 });
-                this.marker = this[v];
-                map.remove(this[v == 'service' ? 'gas' : 'service'])
-                this.$nextTick(_ => {
-                    new AMap.MarkerClusterer(map, this.marker, {
-                        gridSize: 80,
-                        renderClusterMarker: this._renderClusterMarker
-                    });
-                })
+                map.remove(this.marker);
+                this.marker = [];
+                if (v == 'service') {
+                    this.addMarker(map, this.serviceData)
+                }
+                if (v == 'gas') {
+                    this.addMarker(map, this.gasData)
+                }
             }
         },
         mounted() {
             let position = [];
             this.$axios.get('/api/index/list_jtService').then(res => {
+                this.serviceData = JSON.parse(JSON.stringify(res.data.data.servicefrom));
                 position = res.data.data.servicefrom;
                 res.data.data.stationfrom.forEach(i => {
                     if (i.latitude != 1) {
                         i.gas = 'gas';
                         position.push(i);
+                        this.gasData.push(i);
                     }
                 });
                 this.position = position;
