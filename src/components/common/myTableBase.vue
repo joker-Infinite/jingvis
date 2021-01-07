722aa841337d7c1c71161eeef3d02d9403780183
king <2075259561@qq.com>
2021-01-07 11:26:43 +0800

    新公共列表

--- src/components/common/myTableBase.vue
+++ src/components/common/myTableBase.vue
@@ -1,22 +1,43 @@
 <template>
-    <div style="width: 100%;height: 100%">
+    <div style="width: 100%;height: 100%;background: white" class="myTable">
+        <div class="btnBar" v-if="buttons.length>0">
+            <template v-for="btn in buttons">
+                <el-button
+                        size="medium"
+                        :key="btn.name"
+                        v-show="typeof(btn.hidden) == 'undefined'?true:((typeof btn.hidden == 'function')?!btn.hidden():!btn.hidden)"
+                        :type="btn.type?btn.type:'primary'"
+                        :icon="btn.icon?btn.icon:''"
+                        :disabled="typeof(btn.disabled) === 'undefined'?false:((typeof btn.disabled == 'function')?btn.disabled():btn.disabled)"
+                        @click="buttonClick(btn.callback)">
+                    {{btn.name}}
+                </el-button>
+            </template>
+        </div>
         <el-table
                 :data="data"
-                height="100%"
-                :max-height="maxHeight"
+                :height="buttons.length==0&&!pagination?'100%':buttons.length==0||!pagination?'calc(100% - 50px)':'calc(100% - 100px)'"
                 style="width: 100%;"
                 :highlight-current-row="chooseItem == 'single'"
+                row-key="id"
+                :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
                 @select="select"
-                @row-click="selection">
-            <el-table-column v-if="isSelection"
+                @row-click="selection"
+                @select-all="selectAll">
+            <el-table-column v-if="chooseItem != 'single'"
+                             align="center"
                              fixed="left"
                              type="selection"
-                             width="40">
+                             width="50">
             </el-table-column>
             <el-table-column v-if="gridIndex"
+                             align="center"
                              fixed="left"
                              label="序号"
                              width="50">
+                <template slot-scope="scope">
+                    {{scope.$index+1}}
+                </template>
             </el-table-column>
             <template v-for="(item,index) in columns">
                 <el-table-column
@@ -27,7 +48,33 @@
                         :width="item.width ? item.width : ''">
                 </el-table-column>
             </template>
+            <el-table-column
+                    v-if="operations.length>0"
+                    fixed="right"
+                    label="操作"
+                    :width="operationsWidth" align="center">
+                <template slot-scope="scope">
+                    <el-button type="text"
+                               size="small"
+                               v-for="operation in operations"
+                               :key="operation.name"
+                               @click.stop="columnClick(scope.row,scope.$index,operation.callback)">
+                        {{operation.name}}
+                    </el-button>
+                </template>
+            </el-table-column>
         </el-table>
+        <div class="pagination" v-if="pagination">
+            <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage4"
+                    :page-sizes="[100, 200, 300, 400]"
+                    :page-size="100"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="400">
+            </el-pagination>
+        </div>
     </div>
 </template>
 
@@ -43,14 +90,6 @@
                 type: Array,
                 default: []
             },
-            buttons: {
-                type: Array,
-                default: []
-            },
-            maxHeight: {
-                type: String,
-                default: ''
-            },
             chooseItem: {
                 type: String,
                 default: ''
@@ -62,20 +101,106 @@
             gridIndex: {
                 type: Boolean,
                 default: true
+            },
+            buttons: {//网格按钮组
+                type: Array,
+                default: function () {
+                    return []
+                }
+            },
+            operations: {//网格行按钮清单
+                type: Array,
+                default: function () {
+                    return []
+                }
+            },
+            operationsWidth: {
+                type: String,
+                default: ''
+            },
+            pagination: {
+                type: Boolean,
+                default: true
             }
         },
         data() {
-            return {}
+            return {
+                currentPage4: 4
+            }
         },
         methods: {
-            select() {
+            columnClick(item, index, callback) {
+                if (callback) {
+                    callback(item, index);
+                }
             },
-            selection() {
+            buttonClick(callback, $event) {
+                if (callback) {
+                    callback($event);
+                }
+            },
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+            },
+            //多选时点击复选框
+            select(selection, row) {
+                this.$emit('selection-change', selection)
+            },
+            //单选
+            selection(selection) {
+                this.$emit('selection-change', [selection])
+            },
+            //点击全选
+            selectAll(selection) {
+                this.$emit('selection-change', selection)
             }
         }
     }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+    .myTable {
+        .btnBar {
+            width: 100%;
+            height: 50px;
 
+            /deep/ .el-button {
+                padding: 10px 10px;
+                margin: 6px 5px;
+            }
+        }
+
+        /deep/ .el-table {
+            .el-table__header-wrapper {
+                .el-table__header {
+                    tr {
+                        th {
+                            background: #efefef;
+                        }
+                    }
+                }
+            }
+
+            .el-table__fixed-header-wrapper {
+                .el-table__header {
+                    tr {
+                        th {
+                            background: #efefef;
+                        }
+                    }
+                }
+            }
+        }
+
+        .pagination {
+            width: calc(100% - 20px);
+            padding: 9px 10px;
+            height: 32px;
+            text-align: right;
+            overflow: hidden;
+        }
+    }
 </style>
--- src/pages/systemSettings/menuManagement.vue
+++ src/pages/systemSettings/menuManagement.vue
@@ -1,10 +1,76 @@
 <template>
-    <div style="width: 100%;height: 100%"></div>
+    <my-table-base chooseItem="single"
+                   :gridIndex="false"
+                   :operations="operations"
+                   :columns="columns"
+                   operationsWidth="100px"
+                   :data="tableData"
+                   :buttons="buttons"
+                   @selection-change="selectionChange"></my-table-base>
 </template>
 
 <script>
+    import MyTableBase from "../../components/common/myTableBase";
+
     export default {
-        name: "menuManagement"
+        name: "menuManagement",
+        components: {MyTableBase},
+        data() {
+            return {
+                columns: [
+                    {prop: 'a', label: '名称', width: '100px'},
+                    {prop: 'b', label: '图标'},
+                    {prop: 'c', label: '类型'},
+                    {prop: 'd', label: '排序'},
+                    {prop: 'e', label: '路由'},
+                ],
+                tableData: [
+                    {id: 1, a: '111', b: '222', c: '菜单', d: 1, e: '/.../..'},
+                    {id: 2, a: '111', b: '222', c: '菜单', d: 2, e: '/.../..'},
+                    {
+                        id: 3, a: '111', b: '222', c: '菜单', d: 3, e: '/.../..',
+                        children: [
+                            {id: 31, a: '111-2', b: '222-2', c: '子菜单', d: 31, e: '/.../..'},
+                            {id: 32, a: '111-2', b: '222-2', c: '子菜单', d: 32, e: '/.../..'},
+                            {id: 33, a: '111-2', b: '222-2', c: '子菜单', d: 33, e: '/.../..'},
+                        ]
+                    },
+                ],
+                buttons: [
+                    {
+                        name: '新增',
+                        icon: 'el-icon-plus',
+                        callback: _ => {
+                        }
+                    },
+                    {
+                        name: '删除',
+                        icon: 'el-icon-close',
+                        type: 'danger',
+                        callback: _ => {
+                        }
+                    },
+                ],
+                operations: [
+                    {
+                        name: '编辑',
+                        callback: _ => {
+                        }
+
+                    },
+                    {
+                        name: '删除',
+                        callback: _ => {
+                        }
+                    },
+                ]
+            }
+        },
+        methods: {
+            selectionChange(d) {
+                // console.log(d);
+            }
+        }
     }
 </script>
 
