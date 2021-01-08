11795adb56d709fad29b8bdab55d8c5c23cb2b4b
nieshihao <2075259561@qq.com>
2021-01-08 12:56:03 +0800

    权限管理，系统设置

--- src/pages/systemSettings/menuManagement.vue
+++ src/pages/systemSettings/menuManagement.vue
@@ -1,61 +1,61 @@
 <template>
-    <div style="width: 100%;height: 100%">
-        <my-table-base chooseItem="single"
-                       :gridIndex="false"
-                       :operations="operations"
-                       :columns="columns"
-                       operationsWidth="180px"
-                       :data="tableData"
-                       :buttons="buttons"
-                       @selection-change="selectionChange">
-        </my-table-base>
-        <my-dialog width="1000px" title="新增" :visible.sync="visible" :closeOnClickModal="true" height="400px">
-            <el-form :model="formData" label-width="100px">
-                <el-col :span="8">
-                    <el-form-item label="类型：">
-                        <el-select v-model="formData.type">
-                            <el-option label="主菜单" :value="1"></el-option>
-                            <el-option label="子菜单" :value="2"></el-option>
-                        </el-select>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8" v-if="formData.type==2">
-                    <el-form-item label="父菜单">
-                        <el-select v-model="formData.fatherName">
-                            <el-option label="服务区事业部" :value="1"></el-option>
-                            <el-option label="能源事业部" :value="2"></el-option>
-                            <el-option label="传媒指标" :value="3"></el-option>
-                            <el-option label="商业指标" :value="4"></el-option>
-                        </el-select>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="名称：">
-                        <el-input v-model="formData.name"></el-input>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="图标：">
-                        <el-input v-model="formData.icon"></el-input>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="排序：">
-                        <el-input v-model="formData.index"></el-input>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="24">
-                    <el-form-item label="路由：">
-                        <el-input v-model="formData.router"></el-input>
-                    </el-form-item>
-                </el-col>
-            </el-form>
-            <div slot="footerButton">
-                <el-button type="primary">确定</el-button>
-                <el-button type="info" @click="visible=false">取消</el-button>
-            </div>
-        </my-dialog>
-    </div>
+	<div style="width: 100%;height: 100%">
+		<my-table-base chooseItem="single"
+					   :gridIndex="false"
+					   :operations="operations"
+					   :columns="columns"
+					   operationsWidth="180px"
+					   :data="tableData"
+					   :buttons="buttons"
+					   @selection-change="selectionChange">
+		</my-table-base>
+		<my-dialog width="1000px" title="新增" :visible.sync="visible" :closeOnClickModal="true" height="400px">
+			<el-form :model="formData" label-width="100px">
+				<el-col :span="8">
+					<el-form-item label="类型：">
+						<el-select v-model="formData.type">
+							<el-option label="主菜单" :value="1"></el-option>
+							<el-option label="子菜单" :value="2"></el-option>
+						</el-select>
+					</el-form-item>
+				</el-col>
+				<el-col :span="8" v-if="formData.type==2">
+					<el-form-item label="父菜单">
+						<el-select v-model="formData.fatherName">
+							<el-option label="服务区事业部" :value="1"></el-option>
+							<el-option label="能源事业部" :value="2"></el-option>
+							<el-option label="传媒指标" :value="3"></el-option>
+							<el-option label="商业指标" :value="4"></el-option>
+						</el-select>
+					</el-form-item>
+				</el-col>
+				<el-col :span="8">
+					<el-form-item label="名称：">
+						<el-input v-model="formData.name"></el-input>
+					</el-form-item>
+				</el-col>
+				<el-col :span="8">
+					<el-form-item label="图标：">
+						<el-input v-model="formData.icon"></el-input>
+					</el-form-item>
+				</el-col>
+				<el-col :span="8">
+					<el-form-item label="排序：">
+						<el-input v-model="formData.index"></el-input>
+					</el-form-item>
+				</el-col>
+				<el-col :span="24">
+					<el-form-item label="路由：">
+						<el-input v-model="formData.router"></el-input>
+					</el-form-item>
+				</el-col>
+			</el-form>
+			<div slot="footerButton">
+				<el-button type="primary">确定</el-button>
+				<el-button type="info" @click="visible=false">取消</el-button>
+			</div>
+		</my-dialog>
+	</div>
 </template>
 
 <script>
@@ -77,12 +77,11 @@
                     router: '',
                 },
                 columns: [
-                    {prop: 'a', label: '名称', width: '100px', query: true},
+                    {prop: 'a', label: '名称', query: true},
                     {prop: 'b', label: '图标', query: true},
                     {prop: 'c', label: '类型', query: true},
                     {prop: 'd', label: '排序', query: true},
                     {prop: 'e', label: '路由', query: true},
-                    {prop: 'f', label: '授权标识', query: true},
                 ],
                 tableData: [
                     {id: 1, a: '111', b: '222', c: '菜单', d: 1, e: '/.../..'},
@@ -92,7 +91,14 @@
                         children: [
                             {id: 31, a: '111-2', b: '222-2', c: '子菜单', d: 31, e: '/.../..'},
                             {id: 32, a: '111-2', b: '222-2', c: '子菜单', d: 32, e: '/.../..'},
-                            {id: 33, a: '111-2', b: '222-2', c: '子菜单', d: 33, e: '/.../..'},
+                            {
+                                id: 33, a: '111-2', b: '222-2', c: '子菜单', d: 33, e: '/.../..',
+                                children: [
+                                    {id: 331, a: '111-2', b: '222-2', c: '子菜单', d: 31, e: '/.../..'},
+                                    {id: 332, a: '111-2', b: '222-2', c: '子菜单', d: 31, e: '/.../..'},
+                                    {id: 333, a: '111-2', b: '222-2', c: '子菜单', d: 31, e: '/.../..'},
+                                ]
+                            },
                         ]
                     },
                 ],
