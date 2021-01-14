d0cdd27e2292a24de364f9b0cdf7068108507361
king <2075259561@qq.com>
2021-01-14 09:32:02 +0800

    sst

--- src/router/widget/index.js
+++ src/router/widget/index.js
@@ -7,5 +7,11 @@ export default [
         path: "imgParameter",
         name: "imgParameter",
         component: () => import(/* webpackChunkName: "authorityManagement" */ "@/pages/widget/imgParameter.vue")
-    }
+    },
+    {
+        //服务区平面图录入
+        path: "serviceArea",
+        name: "serviceArea",
+        component: () => import(/* webpackChunkName: "authorityManagement" */ "@/pages/widget/serviceArea.vue")
+    },
 ];
